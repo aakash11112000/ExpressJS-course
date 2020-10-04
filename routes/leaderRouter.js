34cd5dbc838a0a6a85c30e1491b6bc895facdccc
Aakash Murugan <aakashmur@gmail.com>
2020-10-05 01:32:59 +0530

    Leaders and promotions REST API endpoints added

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,50 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const dishRouter = express.Router();
-dishRouter.use(bodyParser.json());
+const leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
 
 
 //Chained structure utilized
-dishRouter.route('/')
+leaderRouter.route('/')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
 .get((req, res, next) => {
-    res.end('Will send all the dishes to you!');
+    res.end('Will send all the leaders to you!');
 })
 .post((req,res,next) => {
-    res.end('Will add the dish ' + req.body.name + ' with details ' + req.body.description);
+    res.end('Will add the leader ' + req.body.name + ' with details ' + req.body.description);
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end(req.method + ' operation not supported on: ' + req.url)
 })
 .delete((req,res,next) => {
-    res.end('Deleting all dishes!')
+    res.end('Deleting all leaders!')
 });
 
-dishRouter.route('/:dishId')
+leaderRouter.route('/:leaderId')
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
     next();
 })
 .get((req, res, next) => {
-    res.end('Will send the details of dish with dishId: ' + req.params.dishId + ' to you!');
+    res.end('Will send the details of leader with leaderId: ' + req.params.leaderId + ' to you!');
 })
 .put((req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: '+req.body.name+' with details: '+req.body.description);
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: '+req.body.name+' with details: '+req.body.description);
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end(res.end(req.method + ' operation not supported on: ' + req.url))
 })
 .delete((req, res, next) => {
-    res.end('Will delete the dish with dishId: ' + req.params.dishId);
+    res.end('Will delete the leader with leaderId: ' + req.params.leaderId);
 });
 
-module.exports = dishRouter;
+module.exports = leaderRouter;
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,50 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const dishRouter = express.Router();
-dishRouter.use(bodyParser.json());
+const promoRouter = express.Router();
+promoRouter.use(bodyParser.json());
 
 
 //Chained structure utilized
-dishRouter.route('/')
+promoRouter.route('/')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
 .get((req, res, next) => {
-    res.end('Will send all the dishes to you!');
+    res.end('Will send all the promotions to you!');
 })
 .post((req,res,next) => {
-    res.end('Will add the dish ' + req.body.name + ' with details ' + req.body.description);
+    res.end('Will add a promotion ' + req.body.name + ' with details ' + req.body.description);
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end(req.method + ' operation not supported on: ' + req.url)
 })
 .delete((req,res,next) => {
-    res.end('Deleting all dishes!')
+    res.end('Deleting all promotions!')
 });
 
-dishRouter.route('/:dishId')
+promoRouter.route('/:promoId')
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
     next();
 })
 .get((req, res, next) => {
-    res.end('Will send the details of dish with dishId: ' + req.params.dishId + ' to you!');
+    res.end('Will send the details of promotion with promoId: ' + req.params.promoId + ' to you!');
 })
 .put((req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: '+req.body.name+' with details: '+req.body.description);
+    res.write('Updating the promotion: ' + req.params.promoId + '\n');
+    res.end('Will update the promotion: '+req.body.name+' with details: '+req.body.description);
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end(res.end(req.method + ' operation not supported on: ' + req.url))
 })
 .delete((req, res, next) => {
-    res.end('Will delete the dish with dishId: ' + req.params.dishId);
+    res.end('Will delete the promotion with promoId: ' + req.params.promoId);
 });
 
-module.exports = dishRouter;
+module.exports = promoRouter;
